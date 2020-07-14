ee45bddd464a1be7815690c9035e6e924934cf00
21500266 <andy.park95@gmail.com>
2020-07-14 17:21:48 +0900

    [ADD] add buttoncard & card & listview builder file

---
+++ lib/widgets/card/button_card.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class ButtonCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      child: Center(
+        child: FlatButton(
+          padding: EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              IconButton(
+                icon: Icon(
+                  Icons.add_circle_outline,
+                  color: Colors.black,
+                  size: 35.0,
+                ),
+                onPressed: (){},
+              ),
+              Text(
+                '농사 프로젝트 추가하기',
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 25.0,
+                    color: Colors.black
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
