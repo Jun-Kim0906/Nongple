7457b8ac9a9a217699227a5269964cddbbdd19a1
21500266 <andy.park95@gmail.com>
2020-07-14 17:18:48 +0900

    [UPDATE] modify homescreen

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -4,7 +4,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:nongple/screens/screens.dart';
 import 'package:nongple/blocs/authentication_bloc/authentication_bloc.dart';
 import 'package:nongple/widgets/widgets.dart';
-
+import 'package:intl/intl.dart';
 
 class HomeScreen extends StatelessWidget {
   final String name;
@@ -13,48 +13,86 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-//      appBar: AppBar(),
-        body: Padding(
-          padding: EdgeInsets.fromLTRB(25.0, 0.0, 25.0, 0),
-          child: Container(
-            child: Column(
-              children: [
-                Text('$name 님'),
-                Text('오늘도 풍성한 하루 되세요'),
-                Text(DateTime.now().toString()),
-                Container(
-                  child: Row(
-                    children: [
-                      RaisedButton(),
-                      RaisedButton(),
-                    ],
-                  ),
-                ),
-                Expanded(
-                  child: Container(
+    DateTime now = DateTime.now();
+    String year = DateFormat('yyyy').format(now);
+    String month = DateFormat('MM').format(now);
+    String day = DateFormat('dd').format(now);
+    String weekday = DateFormat('EEEE').format(now);
 
-                  ),
-                ),
-                Card(
-                  child: Center(
-                    child: FlatButton(
-                      child: Column(
-                        children: [
-                          IconButton(
-                            icon: Icon(Icons.add),
-                            onPressed: (){},
-                          ),
-                          Text('농사 프로젝트 추가하기')
-                        ],
-                      ),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(25.0, 0.0, 25.0, 0),
+        child: Container(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                '$name 님',
+                style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
+              ),
+              Text(
+                '오늘도 풍성한 하루 되세요',
+                style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
+              ),
+              Text(
+                '$year 년 $month 월 $day 일 $weekday',
+                style: TextStyle(fontSize: 20.0),
+              ),
+              Container(
+                child: Row(
+//                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ActionChip(
+                        avatar: CircleAvatar(
+//                          backgroundColor: Colors.grey.shade800,
+                          child: Icon(Icons.wifi),
+                        ),
+                        label: Text('RSS'),
+                        onPressed: () {
+                          print(
+                              "RSS");
+                        }),
+                    SizedBox(
+                      width: 20.0,
                     ),
-                  ),
+                    ActionChip(
+                        avatar: CircleAvatar(
+//                          backgroundColor: Colors.grey.shade800,
+                          child: Icon(Icons.settings),
+                        ),
+                        label: Text('설정'),
+                        onPressed: () {
+                          print(
+                              "설정");
+                        }),
+                  ],
                 ),
-              ],
-            ),
+              ),
+              Expanded(
+                child: ListViewBuilder(),
+              ),
+//              Expanded(
+//                child: ListView.builder(
+//                  itemCount: 3,
+//                  itemBuilder: (BuildContext context, int index) {
+//                    if (index == 2) {
+//                      return ButtonCard();
+//                    }
+//                    else {
+//                      return HomePageCard();
+//                    }
+////                    return HomePageCard();
+//                  },
+//                ),
+//              ),
+//              ButtonCard(),
+            ],
           ),
         ),
+      ),
     );
   }
 
