143f8949b12ff83738ccc631a82dfe3005f4df15
21500266 <andy.park95@gmail.com>
2020-07-15 10:00:10 +0900

    [UPDATE] set theme data

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
 
-const mainColor = Color.fromARGB(255, 0, 61, 165);
-const secondColor = Color.fromARGB(51, 67, 193, 170);
-const dividerColor = Color.fromARGB(255, 246, 246, 246);
-Widget divider = Container(
-  height: 8,
-  color: dividerColor,
-);
-const paddingSize = EdgeInsets.fromLTRB(20.0, 0, 20, 0);
-const arrowColor = Color.fromRGBO(0, 0, 0, 0.58);
-const iconColor = Color.fromRGBO(0, 0, 0, 0.54);
-const noDataColor = Color(0xff757575);
+/// throughout all pages
+const mainBackgroundColor = Colors.white;
+
+
+/// facility page
+const facilityGoBackButton = Color.fromARGB(0, 47, 128, 237); // color blue
+const facitityNextButton = Color(0x2F80ED); // color blue
+const facilityTitle = Color(0x000000); // color black
+const faciltySearchTitle = Color(0xBDBDBD); // color gray
+
+
+
+//const mainColor = Color.fromARGB(255, 0, 61, 165);
+//const secondColor = Color.fromARGB(51, 67, 193, 170);
+//const dividerColor = Color.fromARGB(255, 246, 246, 246);
+//Widget divider = Container(
+//  height: 8,
+//  color: dividerColor,
+//);
+//const paddingSize = EdgeInsets.fromLTRB(20.0, 0, 20, 0);
+//const arrowColor = Color.fromRGBO(0, 0, 0, 0.58);
+//const iconColor = Color.fromRGBO(0, 0, 0, 0.54);
+//const noDataColor = Color(0xff757575);
--- lib/widgets/card/button_card.dart
+++ lib/widgets/card/button_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nongple/screens/screens.dart';
 
 class ButtonCard extends StatelessWidget {
   @override
