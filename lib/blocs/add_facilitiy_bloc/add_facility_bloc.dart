56e7381d74f1c4ab9845b6a1a168f6fac62f6230
김준우 <nmnmko04@gmail.com>
2020-07-15 17:21:45 +0900

    [UPDATE] add_state

--- lib/blocs/add_facilitiy_bloc/add_facility_bloc.dart
+++ lib/blocs/add_facilitiy_bloc/add_facility_bloc.dart
@@ -9,10 +9,28 @@ class AddFacilityBloc extends Bloc<AddFacilityEvent, AddFacilityState> {
   Stream<AddFacilityState> mapEventToState(AddFacilityEvent event) async* {
     if(event is FirstPageButtonPressed){
       yield* _mapFirstPageButtonPressedToState(event.facilityName);
+    }else if(event is FacilityNameChanged){
+      yield* _mapFacilityNameChangedToState(event.facilityname);
+    }else if (event is SecondPageButtonPressed){
+      yield* _mapSecondPageButtonPressedToState(event.facilityAddr);
+    }else if (event is FacilityAddrChanged){
+      yield* _mapFacilityAddrChangedToState(event.facilityAddr);
     }
   }
 
   Stream<AddFacilityState> _mapFirstPageButtonPressedToState(String facilityName)async*{
-    yield state.update(facilityName: facilityName);
+    yield state.update(firstPageButtonPressed: true, facilityName: facilityName);
+  }
+
+  Stream<AddFacilityState> _mapFacilityNameChangedToState(String facilityName) async*{
+    yield state.update(isNameValid: facilityName.isNotEmpty);
+  }
+
+  Stream<AddFacilityState> _mapSecondPageButtonPressedToState(String facilityAddr) async*{
+    yield state.update(secondPageButtonPressed: true, facilityAddr: facilityAddr);
+  }
+
+  Stream<AddFacilityState> _mapFacilityAddrChangedToState(String facilityAddr) async*{
+    yield state.update(isAddrValid: facilityAddr.isNotEmpty);
   }
 }
--- lib/blocs/add_facilitiy_bloc/add_facility_state.dart
+++ lib/blocs/add_facilitiy_bloc/add_facility_state.dart
@@ -2,41 +2,69 @@ import 'package:flutter/cupertino.dart';
 
 class AddFacilityState {
   final bool firstPageButtonPressed;
+  final bool isNameValid;
+  final bool secondPageButtonPressed;
+  final bool isAddrValid;
 
   final String facilityName;
+  final String facilityAddr;
 
   AddFacilityState({
     @required this.firstPageButtonPressed,
+    @required this.isNameValid,
+    @required this.secondPageButtonPressed,
+    @required this.isAddrValid,
 
-    @required this.facilityName
+    @required this.facilityName,
+    @required this.facilityAddr,
   });
 
   factory AddFacilityState.empty() {
     return AddFacilityState(
       firstPageButtonPressed: false,
+      isNameValid: false,
+      secondPageButtonPressed: false,
+      isAddrValid: false,
 
       facilityName: '',
+      facilityAddr: '',
     );
   }
 
   AddFacilityState copyWith({
     bool firstPageButtonPressed,
+    bool isNameValid,
+    bool secondPageButtonPressed,
+    bool isAddrValid,
     String facilityName,
+    String facilityAddr,
   }) {
     return AddFacilityState(
       firstPageButtonPressed:
           firstPageButtonPressed ?? this.firstPageButtonPressed,
+      isNameValid: isNameValid ?? this.isNameValid,
+      secondPageButtonPressed: secondPageButtonPressed ?? this.secondPageButtonPressed,
+      isAddrValid: isAddrValid ?? this.isAddrValid,
       facilityName: facilityName ?? this.facilityName,
+      facilityAddr: facilityAddr ?? this.facilityAddr,
     );
   }
 
   AddFacilityState update({
     bool firstPageButtonPressed,
-    String facilityName
+    bool isNameValid,
+    bool secondPageButtonPressed,
+    bool isAddrValid,
+    String facilityName,
+    String facilityAddr,
   }) {
     return copyWith(
       firstPageButtonPressed: firstPageButtonPressed,
+      isNameValid: isNameValid,
+      secondPageButtonPressed: secondPageButtonPressed,
+      isAddrValid: isAddrValid,
       facilityName: facilityName,
+      facilityAddr: facilityAddr,
     );
   }
 
@@ -44,7 +72,11 @@ class AddFacilityState {
   String toString() {
     return '''AddFacilityState{
     firstPageButtonPressed: $firstPageButtonPressed,
+    isNameValid: $isNameValid,
+    secondPageButtonPressed: $secondPageButtonPressed,
+    isAddrValid: $isAddrValid,
     facilityName: $facilityName,
+    facilityAddr: $facilityAddr,
     ''';
   }
 }
