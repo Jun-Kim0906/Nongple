0068e42030b1ca1de56cd26e4753f4ead08c313b
김준우 <nmnmko04@gmail.com>
2020-07-20 16:03:58 +0900

    [DELETE] comment

--- lib/blocs/home_bloc/home_state.dart
+++ lib/blocs/home_bloc/home_state.dart
@@ -9,58 +9,6 @@ class HomeState {
 
   @override
   List<Object> get props => [];
-//  final bool settingBtnPressed;
-//  final bool customBackgroundImgSelected;
-//  final String backgroundImg;
-//
-//  HomeState({
-//    @required this.settingBtnPressed,
-//    @required this.customBackgroundImgSelected,
-//    @required this.backgroundImg,
-//  });
-//
-//  factory HomeState.empty() {
-//    return HomeState(
-//      settingBtnPressed: false,
-//      customBackgroundImgSelected: false,
-//      backgroundImg: "",
-//    );
-//  }
-//
-//  HomeState update({
-//    bool settingBtnPressed,
-//    bool customBackgroundImgSelected,
-//    String backgroundImg,
-//  }) {
-//    return copyWith(
-//      settingBtnPressed: settingBtnPressed,
-//      customBackgroundImgSelected: customBackgroundImgSelected,
-//      backgroundImg: backgroundImg,
-//    );
-//  }
-//
-//  HomeState copyWith({
-//    bool settingBtnPressed,
-//    bool customBackgroundImgSelected,
-//    String backgroundImg,
-//  }) {
-//    return HomeState(
-//      settingBtnPressed: settingBtnPressed ?? this.settingBtnPressed,
-//      customBackgroundImgSelected: customBackgroundImgSelected ?? this.customBackgroundImgSelected,
-//      backgroundImg: backgroundImg ?? this.backgroundImg,
-//    );
-//  }
-//
-//  @override
-//  String toString(){
-//    return '''
-//    HomeState{
-//      settingBtnPressed: $settingBtnPressed,
-//      customBackgroundImgSelected: $customBackgroundImgSelected,
-//      backgroundImg: $backgroundImg,
-//    }
-//    ''';
-//  }
 }
 
 class Initial extends HomeState{}
