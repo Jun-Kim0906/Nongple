27114e5b47d594756bcadf250130e490d4e8866b
21500266 <andy.park95@gmail.com>
2020-07-21 17:19:38 +0900

    [UPDATE] modify weather screen ui

--- lib/screens/weather/weather_screen.dart
+++ lib/screens/weather/weather_screen.dart
@@ -1,7 +1,11 @@
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:nongple/blocs/blocs.dart';
+import 'package:nongple/utils/utils.dart';
+import 'package:nongple/widgets/custom_icons/custom_icons.dart';
 import 'package:nongple/widgets/loading/Loading.dart';
+import 'package:intl/intl.dart';
 
 class WeatherScreen extends StatefulWidget {
   @override
@@ -21,38 +25,114 @@ class _WeatherScreenState extends State<WeatherScreen> {
 
   @override
   Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
     return BlocBuilder<WeatherBloc, WeatherState>(
-      builder: (context, state){
+      builder: (context, state) {
         if (state is WeatherListSet) {
-          print(state.weatherList);
           return Padding(
             padding: EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 0.0),
-            child: ListView.separated(
-              itemCount: state.weatherList.length,
-              separatorBuilder: (BuildContext context, int index) => Divider(
-                thickness: 1.0,
-              ),
-              itemBuilder: (BuildContext context, int index) {
-                return ListTile(
-                  title: Column(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  '$year년 $month월 $day일',
+                  style: TextStyle(fontSize: 20.0),
+                ),
+                Container(
+                  child: Column(
                     children: [
-                      Text(state.weatherList[index].baseDate),
-                      Text(state.weatherList[index].baseTime),
-                      Text(state.weatherList[index].category),
-                      Text(state.weatherList[index].fcstDate),
-                      Text(state.weatherList[index].fcstTime),
-                      Text(state.weatherList[index].fcstValue),
-                      Text(state.weatherList[index].nx.toString()),
-                      Text(state.weatherList[index].ny.toString()),
+                      Container(
+                        padding: EdgeInsets.fromLTRB(0.0, 20.0, 0.0, 20.0),
+                        child: Icon(
+                          CustomIcons.sun,
+                          size: 100,
+                          color: Colors.yellow[700],
+                        ),
+                      ),
+                      Container(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Column(
+                              children: [
+                                Text(
+                                  '현재기온',
+                                  style: TextStyle(color: Colors.blue[600]),
+                                ),
+                                Text(
+                                  '25' + degrees + 'C',
+                                  style: TextStyle(
+                                      fontSize: 35.0,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ],
+                            ),
+                            Column(
+                              children: [
+                                Text(
+                                  '습도',
+                                  style: TextStyle(color: Colors.blue[600]),
+                                ),
+                                Text(
+                                  '90%',
+                                  style: TextStyle(
+                                      fontSize: 35.0,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                      Divider(
+                        thickness: 1.0,
+                      ),
                     ],
                   ),
-                );
-              },
-            )
+                ),
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: state.tmpList.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      String fcstTime = state.tmpList[index].fcstTime;
+                      return ListTile(
+                        leading: Text(fcstTime[0] + fcstTime[1] + '시'),
+                        title: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            (int.parse(state.skyList[index].fcstValue) <= 5)
+                                ? Icon(CustomIcons.sun, color: Colors.yellow[700],)
+                                : (int.parse(state.skyList[index].fcstValue) >
+                                            5 &&
+                                        int.parse(
+                                                state.skyList[index].fcstValue) <
+                                            9)
+                                    ? Icon(CustomIcons.cloud, color: Colors.grey[400],)
+                                    : Icon(CustomIcons.rain, color: Colors.blue[600],),
+                            SizedBox(width: width * 0.008,),
+                            Text(state.tmpList[index].fcstValue + degrees + 'C'),
+                          ],
+                        ),
+                        trailing: Container(
+                          width: width * 0.09,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              Text(state.humidList[index].fcstValue + '%'),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
           );
-        }
-        else {
-          return Center(child: Loading(),);
+        } else {
+          return Center(
+            child: Loading(),
+          );
 //          return Container(
 //            child: Center(
 //              child: Text("Weather Info Not Available", style: TextStyle(fontSize: 25),),
--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -3,3 +3,4 @@ export 'style.dart';
 export 'todays_date.dart';
 export 'uicode.dart';
 export './user_util/user_util.dart';
+export 'weather_util/api_addr.dart';
